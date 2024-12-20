591a2aaddbb0ceedb51a26c038830a81ce9385ad
yousefh2004 <114005518+yousefh2004@users.noreply.github.com>
2024-12-20 18:10:15 +0200

    Add files via upload

---
+++ N1.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+<div className="navbar">
+  <div className="container">
+    <div className="logo">
+      <a href="#">Start Bootstrap</a>
+    </div>
+    <div>
+        <div className='uui'>
+          <a href="#two">Portfolio</a> 
+          <a href="#three">About</a> 
+          <a href="#five">Contact</a>  
+        </div>
+    </div>
+  </div>
+</div>
+
+
+
+    </>
+  )
+}
